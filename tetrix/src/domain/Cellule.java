4ea04f67c67482cdf3dcfc588a371ad04946081c
herve nathan <djoumessinathan@gmail.com>
2019-04-09 13:05:36 +0100

    commit 1

---
+++ tetrix/src/domain/Cellule.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Cellule {
+	private int posX;
+	private int posY;
+
+}
--- tetrix/src/tetrix/launcher.java
+++ tetrix/src/tetrix/launcher.java
@@ -1,9 +1,12 @@
 package tetrix;
 
+import java.io.Console;
+
 public class launcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello world");
 
 	}
 
